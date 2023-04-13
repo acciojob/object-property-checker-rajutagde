5f26187b469c88b5306877ab8d9ad6a0cdd4ca60
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-13 22:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return key in sampleObject;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Example usage:
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
